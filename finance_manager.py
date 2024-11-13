8e0bee41a2e31c5245f05432d5300e83441d336b
bakuwaku <emanukya@terpmail.umd.edu>
2024-11-12 19:01:30 -0500

    Update finance_manager.py

--- finance_manager.py
+++ finance_manager.py
@@ -5,5 +5,26 @@ Assignment: Project
 Date: 11/11/2024
 
 Challenges Encountered: 
-"""
 
+class Person:
+    """Represents a user of the financial app who can track expenses, income, and savings."""
+
+    def __init__(self, username: str):
+        """Initializes the financial tracker with user details and empty lists for expenses, income, and savings.
+
+        Args:
+            username (str): The username of the person.
+        """
+    
+    def register_user(self):
+        """Registers a new user by creating an instance if one does not exist.
+
+        Attributes:
+            username (str): The username associated with this account.
+            expenses (list): A list of expenses associated with the user.
+            income (float): The user's income.
+            savings (float): The user's total savings.
+        """
+    
+    def display_information(self):
+        """Displays the financial information of the user."""
